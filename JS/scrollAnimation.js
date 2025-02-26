5c7b9b7cae2c1a313277166227fc5a6fd4e1d878
Emerson <emersonog066@gmail.com>
2025-02-26 12:00:49 -0300

    atualização

---
+++ JS/scrollAnimation.js
@@ -0,0 +1,22 @@
+const menuLinks = document.querySelectorAll(".header-menu a");
+
+function getDistanceFromTheTop(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+function nativeScroll(distanceFromTheTop) {
+  window.scroll({
+    top: distanceFromTheTop, behavior: "smooth",
+  });
+}
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const distanceFromTheTop = getDistanceFromTheTop(event.target);
+  nativeScroll(distanceFromTheTop);
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
