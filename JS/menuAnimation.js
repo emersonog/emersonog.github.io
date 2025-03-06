e151d80c07184807dab12fd20555d7f3ce9b9bdc
Emerson <emersonog066@gmail.com>
2025-03-06 13:18:06 -0300

    menu mobile

--- JS/menuAnimation.js
+++ JS/menuAnimation.js
@@ -2,7 +2,7 @@ let navBar = document.querySelector('#header')
 
 document.addEventListener('scroll', () => {
   let scrollTop = window.scrollY
-  if (scrollTop > 0) {
+  if (scrollTop > 1) {
     navBar.classList.add('rolar')
   } else {
     navBar.classList.remove('rolar')
@@ -11,8 +11,21 @@ document.addEventListener('scroll', () => {
 let btnMenuMob = document.querySelector('#btn-menu-mob')
 let line1 = document.querySelector('.line-menumob-1')
 let line2 = document.querySelector('.line-menumob-2')
+let menuMobile = document.querySelector('#menu-mobile')
+let opcaoMobile = document.querySelector('#opcao-mobile')
+
+let body = document.querySelector('body')
 
 btnMenuMob.addEventListener("click", () => {
   line1.classList.toggle('ativo1')
   line2.classList.toggle('ativo2')
-})
+  menuMobile.classList.toggle('abrir')
+
+  body.classList.toggle('no-overflow')
+});
+opcaoMobile.addEventListener("click", () => {
+  line1.classList.toggle('ativo1')
+  line2.classList.toggle('ativo2')
+  menuMobile.classList.toggle('abrir')
+});
+
